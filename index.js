9071f13243e9418b99a94fb1e7b4b8d80821d860
Sham Ashok Gulajkar <155843141+Gulajkar-sham23@users.noreply.github.com>
2025-02-04 18:09:29 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express');
 const app = express();
 const PORT = 3000;
-app.get('/', (req, res) => res.send('Hello from DevOps teams !'));
+app.get('/', (req, res) => res.send('Hello from DevOps teams ! your server is now running'));
 app.listen(PORT, () => console.log(`node app is  running on port ${PORT}`));
